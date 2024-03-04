3ecf224103ae48585414b113f8a8cb565c2ae636
寺澤徳隆 <mosqey1889@gmail.com>
2024-03-04 22:28:10 +0900

    MainFragmentの設定

---
+++ app/src/main/java/com/example/weatherapp/ui/main/MainFragment.kt
@@ -0,0 +1,25 @@
+package com.example.weatherapp.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.weatherapp.R
+
+class MainFragment : Fragment() {
+
+    // static領域みたいな
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_main,container,false)
+        return view
+    }
+}
